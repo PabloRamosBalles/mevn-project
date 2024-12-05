d0574f035cb63a4545cf9a4ef3aaae51c95258a6
pabloramosballes <pablo.ramos.balles@gmail.com>
2024-12-05 09:28:51 +0100

    Creacion-EndPoint-Register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="****" v-model="password">
+    <br>
+    <button @click="register">Register</button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '../services/AuthenticationService'
+export default {
+  name: 'register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
